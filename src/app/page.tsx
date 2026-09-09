5b356f12b3b5dcac79855a993af41c5eaa6da17d
kasraam <kasraam385@gmail.com>
2026-09-10 00:14:43 +0330

    feat(components): add card & items component from shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,12 @@ export default function Home() {
   return (
     <div className="px-4">
       <HeaderContainer />
+      <section className="mt-12 grid gap-6 grid-cols-9">
+        <div>grid one</div>
+        <div>grid one</div>
+        <div>grid one</div>
+        <div>grid one</div>
+      </section>
     </div>
   );
 }
