85b2e387e5f0bb05ce1d0433888625f40fc01e52
kasraam <kasraam385@gmail.com>
2026-09-10 14:02:09 +0330

    feat(fetch chats): add api call + fetch in chat box UI

---
+++ src/app/api/guestbook/route.ts
@@ -0,0 +1,27 @@
+import dbConnect from "@/lib/db";
+import { Guestbook } from "@/schema/guest-book";
+
+export async function GET() {
+  try {
+    await dbConnect();
+
+    const entries = await Guestbook.find({}).sort({ createdAt: -1 }).lean();
+
+    return new Response(JSON.stringify(entries), {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch (err) {
+    console.error(err);
+    return new Response(
+      JSON.stringify({
+        success: false,
+        error: "Something went wrong. Please try again later.",
+      }),
+      {
+        status: 500,
+        headers: { "Content-Type": "application/json" },
+      },
+    );
+  }
+}
--- src/components/works/container.tsx
+++ src/components/works/container.tsx
@@ -6,7 +6,7 @@ import WorkItem from "./work-item";
 export default function WorkExpContainer() {
   const works = sortWorksDesc(worksExp.worksExp);
   return (
-    <Card className="bg-transparent relative overflow-visible ring-border">
+    <Card className="max-h-80 h-80 bg-transparent relative overflow-visible ring-border">
       <Card className="text-sm font-semibold absolute px-3 py-1 bg-background -top-3.5 left-6 ring-border">
         Works
       </Card>
