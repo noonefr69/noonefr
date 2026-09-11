1da1a093519daf69a804bd5e9d54c68adfa199c9
kasraam <kasraam385@gmail.com>
2026-09-12 00:42:14 +0330

    refactor(themes): change theme and remove mode toggle it is only dark mode

--- src/components/footer/container.tsx
+++ src/components/footer/container.tsx
@@ -1,6 +1,5 @@
 import { socials } from "@/data/socials";
 import FooterItem from "./item";
-import { ModeToggle } from "../toggle-mode";
 
 export default function FooterContainer() {
   return (
@@ -9,7 +8,6 @@ export default function FooterContainer() {
         © {new Date().getFullYear()}
       </div>
       <div className="flex items-center gap-2">
-        <ModeToggle />
         {socials.map((social) => (
           <FooterItem key={social.url} social={social} />
         ))}
